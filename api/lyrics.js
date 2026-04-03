db400316b1944899605a2519f6ec803e6e3128d7
niteoowl <xx.niteowl@gmail.com>
2026-04-03 19:19:40 +0900

    Update lyrics.js

--- api/lyrics.js
+++ api/lyrics.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,4 +22,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
-}
+};
